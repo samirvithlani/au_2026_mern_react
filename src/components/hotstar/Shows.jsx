107e75ee95eb79b3d2cb26de64c27f48defd73d8
samirvithlani <samir.vithlani83955@gmail.com>
2026-08-02 12:06:16 +0530

    team

--- src/components/hotstar/Shows.jsx
+++ src/components/hotstar/Shows.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Shows = () => {
+  const showList = [
+    {id:1,name:"money heist"},
+    {id:2,name:"breaking BAD"},
+    {id:3,name:"squid game"}
+  ]
   return (
     <div style={{textAlign:"center"}}>
         <h1>Shows</h1>
+        {
+          showList.map((show)=>{
+            return <li>
+              <Link to ={`/watch/${show.name}`}>{show.name}</Link>
+            </li>
+          })
+        }
     </div>
   )
 }
